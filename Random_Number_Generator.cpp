cf42de3085ec0b5482236e918c712116495e6d0e
Tripp Hopkins <tripphopkins13245@gmail.com>
2024-07-05 11:21:50 -0400

    Added base generator code

--- Random_Number_Generator.cpp
+++ Random_Number_Generator.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 int main(){
+    // pseudo-random = NOT truely random (but close)
 
+    srand(time(NULL));
 
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
+
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
 
     return 0;
 }
